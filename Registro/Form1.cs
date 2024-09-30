4a71b7f41c748febedb4ec85aba221d82cd18a7e
MichaelCasco4 <mecascov@uamv.edu.ni>
2024-09-30 08:48:27 -0600

    New Proyect Register

---
+++ Registro/Form1.cs
@@ -0,0 +1,52 @@
+﻿using Registro.models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Registro
+{
+    public partial class Form1 : Form
+    {
+        List<Person> persons; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            persons = new List<Person>();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Person person = new Person();
+            person.FirstName = tbFirstName.Text;
+            person.LastName = tbLastName.Text;
+            person.Birthdate = dtpBirthdate.Value; 
+
+            persons.Add(person);
+
+            ShowPersons();
+            ClearTb();
+        }
+
+        private void ShowPersons()
+        {
+            dgvPersons.DataSource = null; 
+            dgvPersons.DataSource = persons;
+        }
+
+        private void ClearTb()
+        {
+            tbFirstName.Clear();
+            tbLastName.Clear();
+            dtpBirthdate.Value = DateTime.Now; 
+            tbFirstName.Focus();
+
+        }
+    }
+}
---
+++ Registro/models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Registro.models
+{
+    public struct Person
+    {
+        public string FirstName {  get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+}
